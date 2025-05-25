661e9939884b667af6a4b465111825d0fe678665
mli <solmichaelk@hotmail.com>
2025-05-25 14:40:16 -0400

    fix jwplayer CC display issue

--- load_caption.php
+++ load_caption.php
@@ -1,5 +1,20 @@
-<?php 
+<?php
 require_once('../../config.php');
+
+function encodeUrl($url) {
+    if (urldecode($url) !== $url) {
+        return $url; // Return as-is if already encoded
+    }
+
+    return preg_replace_callback(
+        '/%2F|%3A/',  // Match encoded `/` (%2F) and `:` (%3A)
+        function ($matches) {
+            return urldecode($matches[0]); // Decode them back
+        },
+        rawurlencode($url)
+    );
+}
+
 $file = $_GET['file'];
-$content = file_get_contents($file);
+$content = file_get_contents(encodeUrl($file));
 echo $content;
